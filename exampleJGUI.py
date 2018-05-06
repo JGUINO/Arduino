a52546cf8c0f38bc49f380c02a926f7269d7b9ed
Jérôme GUILLON <jerome.guillon@cmc-coffrages.com>
2018-05-06 19:09:32 +0200

    debug MQTT

--- exampleJGUI.py
+++ exampleJGUI.py
@@ -130,7 +130,7 @@ class affichageOLED:
 
 try:
 	d=affichageOLED()
-    p=publication()
+	p=publication()
 	# Create an object hx which represents your real hx711 chip
 	# Required input parameters are only 'dout_pin' and 'pd_sck_pin'
 	# If you do not pass any argument 'gain_channel_A' then the default value is 128
