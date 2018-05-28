481c44ce459bc74ee442d7537bfaa573f05e2323
Jérôme GUILLON <jerome.guillon@cmc-coffrages.com>
2018-05-28 14:38:31 +0200

    tabulations

--- exampleJGUI.py
+++ exampleJGUI.py
@@ -70,8 +70,8 @@ print("avant connection "+hostMQTT)
 client.connect(hostMQTT, 1883, 60)
 print("apres connection")
 if client.connecte==True:
-    client.loop_start()
-    client.publish("capteurs/pression"+nomCapteur, "Demarrage capteur ", qos=0, retain=False)
+	client.loop_start()
+	client.publish("capteurs/pression"+nomCapteur, "Demarrage capteur ", qos=0, retain=False)
 
 # fonctions de publication
 def publier(client, message):
