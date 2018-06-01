a12573c2ec18f921a8e8495706c5354034a45472
JGUINO <jerome.guillon@cmc-coffrages.com>
2018-06-01 15:01:41 +0200

    cmd

--- chipcommande.py
+++ chipcommande.py
@@ -78,7 +78,7 @@ class coffrage():
 
 fenetre = tk.Tk()
 buton=[]
-for i in [coffrage,pieds]:
+for i in [coffrage]:
     buton.append(tk.Button(fenetre,text=str(i),command=lambda:activer(i)))
 for i in buton:
     i.pack()
