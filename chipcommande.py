af453128a24e502ce54d22dddefd6e3a32a31133
JGUINO <jerome.guillon@cmc-coffrages.com>
2018-06-01 16:08:14 +0200

    cmd

--- chipcommande.py
+++ chipcommande.py
@@ -72,6 +72,7 @@ class coffrage:
             #self.i = i
             #GPIO.setup(self.i,GPIO.OUT)
             #GPIO.output(self.i,False)
+
 def activer(x):
     #GPIO.output(self.x,True)
     print("Activation de "+str(x))
