f122ee7c1885045a4726a3eb2f0b2137d0c84d9b
JGUINO <jerome.guillon@cmc-coffrages.com>
2018-06-04 16:19:31 +0200

    cmd

--- chipcommande.py
+++ chipcommande.py
@@ -208,12 +208,12 @@ crt1.place(x=505,y=110)
 crt2.place(x=505,y=130)
 
 quitter=tk.Button(fenetre,text='Quitter',command=fenetre.destroy)
-quitter.place(x=730,y=400)
+quitter.place(x=725,y=430)
 
 for i in boutons:
     i.config(state=NORMAL)
 
-bcoffr=tk.Button(fenetre,text='Coffrage',font=(50))
+bcoffr=tk.Button(fenetre,text='Coffrage',font=(70))
 bcoffr.place(x=100,y=430)
 
 fenetre.geometry("800x600+10+10")
