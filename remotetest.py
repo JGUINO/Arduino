cc95fa9b9f22b589594521bf1a463157db9c3a82
JGUINO <jerome.guillon@cmc-coffrages.com>
2018-06-06 15:36:02 +0200

    rmt

--- remotetest.py
+++ remotetest.py
@@ -2,9 +2,8 @@ from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import Motor
 from gpiozero import LED
 from time import sleep
-#factory = PiGPIOFactory(host='192.168.1.124')
-factory3 = PiGPIOFactory(host='192.168.1.3')
-red=LED(7,25,pin_factory=factory3)
+factory = PiGPIOFactory(host='192.168.1.124')
+red=LED(7,25,pin_factory=factory)
 
 while True:
     red.on()
