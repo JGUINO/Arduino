27245eb50a0f81436a14ecf20ccf3595fb69a663
JGUINO <jerome.guillon@cmc-coffrages.com>
2018-06-07 11:11:57 +0200

    rmt

--- chipcommande.py
+++ chipcommande.py
@@ -2,74 +2,133 @@ import RPi.GPIO as GPIO		# import GPIO
 #import paho.mqtt.client as mqtt
 from tkinter import *
 import tkinter as tk
+from gpiozero.pins.pigpio import PiGPIOFactory
+
 fenetre = tk.Tk()
 downarrow=PhotoImage(file='arrowdown.gif')
 uparrow=PhotoImage(file='arrowup.gif')
 
-c=['IOoutlfu','IOoutlfd','IOoutrfu','IOoutrfd','IOoutlmu','IOoutlmd','IOoutrmu','IOoutrmd','IOoutltu','IOoutltd','IOoutrtu','IOoutrtd']
-pieds=['IOoutup1','IOoutdo1','IOoutup2','IOoutdo2','IOoutup3','IOoutdo3','IOoutup4','IOoutdo4']
+factory = PiGPIOFactory(host='192.168.1.117')
 
-class pied():
-    def __init__(self,pieds):
-        self.IOoutup1=IOoutup1
-        self.IOoutdo1=IOoutdo1
-        self.IOoutup2=IOoutup2
-        self.IOoutdo2=IOoutdo2
-        self.IOoutup3=IOoutup3
-        self.IOoutdo3=IOoutdo3
-        self.IOoutup4=IOoutup4
-        self.IOoutdo4=IOoutdo4
-
-        GPIO.setmode(GPIO.BCM)
-
-        GPIO.setup(self.IOoutup1,GPIO.OUT)
-        GPIO.output(self.IOoutup1, False)
-        GPIO.setup(self.IOoutdo1,GPIO.OUT)
-        GPIO.output(self.IOoutdo1, False)
-        GPIO.setup(self.IOoutup2,GPIO.OUT)
-        GPIO.output(self.IOoutup2, False)
-        GPIO.setup(self.IOoutdo2,GPIO.OUT)
-        GPIO.output(self.IOoutdo2, False)
-        GPIO.setup(self.IOoutup3,GPIO.OUT)
-        GPIO.output(self.IOoutup3, False)
-        GPIO.setup(self.IOoutdo3,GPIO.OUT)
-        GPIO.output(self.IOoutdo3, False)
-        GPIO.setup(self.IOoutup4,GPIO.OUT)
-        GPIO.output(self.IOoutup4, False)
-        GPIO.setup(self.IOoutdo4,GPIO.OUT)
-        GPIO.output(self.IOoutdo4, False)
-
-    def up1():
-        GPIO.output(self.IOoutup, True)
-        print("Activation up1")
-    def down1():
-        GPIO.output(self.IOoutdo, True)
-        print("Activation down1")
-    
-    def up2():
-        GPIO.output(self.IOoutup, True)
-        print("Activation up2")
-    def down2():
-        GPIO.output(self.IOoutdo, True)
-        print("Activation down2")
-
-    def up3():
-        GPIO.output(self.IOoutup, True)
-        print("Activation up3")
-    def down3():
-        GPIO.output(self.IOoutdo, True)
-        print("Activation down3")
-
-    def up4():
-        GPIO.output(self.IOoutup, True)
-        print("Activation up4")
-    def down4():
-        GPIO.output(self.IOoutdo, True)
-        print("Activation down4")
-
+coffrage=[]
+pieds=[]
 boutons=[]
-boutonscoffrage=[]
-boutonspieds=[]
+
+
+class bouton():
+    def __init__(self,,nom,sortie,ip,typ):
+        self.sortie=sortie
+        self.ip=ip
+        self.typ=typ
+        self.nom=nom
+        self.dire=dire
+        GPIO.setmode(GPIO.BCM)
+        self.button=tk.Button(fenetre)
+        if self.dire==up:
+            button.config(image=uparrow)
+        elif self.dire==do:
+            button.config(image=downarrow)
+        button.bind('<ButtonPress-1>',lambda event:nom.activer())
+        button.bind('<ButtonRelease-1>',lambda event:nom.release())
+
+        if type(self.sortie)==list:
+            for i in sortie:
+                GPIO.setup(self.i,GPIO.OUT)
+                GPIO.output(self.i, False)
+
+    def press(self):
+        if type(self.sortie)==list:
+            for i in sortie:
+                GPIO.output(self.i,True)
+        else:
+            GPIO.output(self.sortie, True)
+        print('Activation de {}'.format(self.sortie))
+        framec.config(bg='red')
+        framep.config(bg='red')
+        f1.config(bg='red')
+        b1.config(bg='red')
+        tt.config(bg='red')
+        for i in boutons:
+            if i.nom!=self.nom:
+                i.config(state=DISABLED)
+        
+        def release(self):
+            framec.config(bg='green')
+            framep.config(bg='blue')
+            f1.config(bg='blue')
+            b1.config(bg='blue')
+            tt.config(bg='blue')
+            for j in clab:
+                j.config(bg='green')
+            for k in plab:
+                k.config(bg='green')
+            for i in boutons:
+                i.button.config(state=NORMAL)
+
+
+pfw1=pied(,,,pied)
+pieds.append(pfw1)
+pbw1=pied(,,,pied)
+pieds.append(pbw1)
+pfw2=pied(,,,pied)
+pieds.append(pfw2)
+pbw2=pied(,,,pied)
+pieds.append(pbw2)
+pfw3=pied(,,,pied)
+pieds.append(pfw3)
+pbw3=pied(,,,pied)
+pieds.append(pbw3)
+pfw4=pied(,,,pied)
+pieds.append(pfw4)
+pbw4=pied(,,,pied)
+pieds.append(pbw4)
+pfw=pied(,,,pied)
+pbw=pied(,,,pied)
+pieds.append(pfw)
+pieds.append(pbw)
+
+clfu=bouton(lfu,,,coffrage)
+clfd=bouton(lfd,,,coffrage)
+crfu=bouton(rfu,,,coffrage)
+crfd=bouton(rfd,,,coffrage)
+clmu=bouton(lmu,,,coffrage)
+clmd=bouton(lmd,,,coffrage)
+crmu=bouton(rmu,,,coffrage)
+crmd=bouton(rmd,,,coffrage)
+clbu=bouton(lbu,,,coffrage)
+clbd=bouton(lbd,,,coffrage)
+crbu=bouton(rbu,,,coffrage)
+crbd=bouton(rbd,,,coffrage)
+cfu=bouton(fu,,,coffrage)
+cfd=bouton(fd,,,coffrage)
+cbu=bouton(bu,,,coffrage)
+cbd=bouton(bd,,,coffrage)
+cup=bouton(up,,,coffrage)
+cdo=bouton(do,,,coffrage)
+
+coffrage.append(clfu)
+coffrage.append(clfd)
+coffrage.append(crfu)
+coffrage.append(crfd)
+coffrage.append((clmu)
+coffrage.append(clmd)
+coffrage.append(crmu)
+coffrage.append(crmd)
+coffrage.append(clbu)
+coffrage.append(clbd)
+coffrage.append(crbu)
+coffrage.append(crbd)
+coffrage.append(cfu)
+coffrage.append(cfd)
+coffrage.append(cbu)
+coffrage.append(cbd)
+coffrage.append(cup)
+coffrage.append(cdo)
+
+for i in coffrage:
+    boutons.append(i)
+for i in pieds:
+    boutons.append(i)
 
 framec=Frame(fenetre,height=190,width=720,bg="green")
 framec.place(x=0,y=30)
@@ -92,6 +151,10 @@ clt2=Label(text="gauche",bg='green')
 crt1=Label(text="Arrière",bg='green')
 crt2=Label(text="droit",bg='green')
 clab=[clf1,clf2,crf1,crf2,clm1,clm2,crm1,crm2,clt1,clt2,crt1,crt2]
+f1=Label(text="Avant",bg='blue')
+b1=Label(text='Arrière',bg='blue')
+tt=Label(text='Tous',bg='blue')
+
 
 pied1=Label(text="Pied 1",bg="green")
 pied2=Label(text="Pied 2",gb="green")
@@ -99,47 +162,9 @@ pied3=Label(text="Pied 3",bg="green")
 pied4=Label(text="Pied 4",bg="green")
 plab=[pied1,pied2,pied3,pied4]
 
-class coffrage:
-    #def __init__(self,c):
-        #for i in c:
-            #self.i = i
-            #GPIO.setup(self.i,GPIO.OUT)
-            #GPIO.output(self.i,False)
-    def activer(self,nom,bout):
-        self.bout=bout
-        self.nom=nom
-        global boutons
-        global pieds
-        global plab
-        #GPIO.output(self.x,True)
-        print("Activation de "+str(nom))
-        if nom in c:
-            framec.config(bg='red')
-            for j in clab:
-                j.config(bg='red')
-        if nom in pieds:
-            framep.config(bg='red')
-            for k in plab:
-                k.config(bg='red')
 
-        for i in boutons:
-            if i!=bout:
-                i.config(state=DISABLED)
 
-    def release(self,nom):
-        global boutons
-        global clab
-        global plab
-        self.nom=nom
-        framec.config(bg='green')
-        framep.config(bg='blue')
-        for j in clab:
-            j.config(bg='green')
-        for k in plab:
-            k.config(bg='green')
-        print(str(nom)+' désactivé')
-        for i in boutons:
-            i.config(state=NORMAL)
+
 
 d=0
 
@@ -154,12 +179,15 @@ def affichercoffrage():
     global clab
     bcoffr.config(bg='yellow')
     bpie.config(bg='grey')
+    f1.place_forget()
+    b1.place_forget()
+    tt.place_forget()
     for i in boutons:
         i.place_forget()
     for j in plab:
         j.place_forget()
-    for k in boutonscoffrage:
-        p=k
+    for k in coffrage:
+        p=k.button
         if n%2==1:
             p.place(x=a,y=o)
         elif n%2==0:
@@ -167,10 +195,13 @@ def affichercoffrage():
             a=a+100
         n=n+1
         if n==13:
-            o=o+100
+            o=o+200
             a=10
     u=0
     n=0
+    f1.place(x=5,y=300)
+    b1.place(x=105,y=300)
+    tt.place(x=205,y=300)
     for cl in clab:
         
         if n%2==0:
@@ -179,7 +210,7 @@ def affichercoffrage():
             cl.place(x=5+u*100,y=120)
             u=u+1
         n=n+1
-    fenetre.mainloop()
+
 
     
 
@@ -191,7 +222,9 @@ def afficherpieds():
     global plab
     bpie.config(bg='yellow')
     bcoffr.config(bg='grey')
-
+    f1.place_forget()
+    b1.place_forget()
+    tt.place_forget()
     for i in boutons:
         i.place_forget()
     for j in clab:
@@ -208,57 +241,15 @@ def afficherpieds():
             o=o+200
             a=10
     u=0
+    f1.place(x=5,y=300)
+    b1.place(x=105,y=300)
+    tt.place(x=205,y=300)
     for p in plab:
         p.place(x=10+u*100,y=110)
         u=u+100
-    fenetre.mainloop()
 
 
 
-n=1
-l=0
-h=0
-c3189=coffrage()
-
-
-for i in c:
-    p=i
-    if n%2==1:
-        p=tk.Button(fenetre)
-        p.config(image=uparrow)
-        p.bind('<ButtonPress-1>',lambda event, p=p,nom=i:c3189.activer(nom,p))
-        p.bind('<ButtonRelease-1>',lambda event, nom=i:c3189.release(nom))
-        
-        
-    elif n%2==0:
-        p=tk.Button(fenetre)
-        p.config(image=downarrow)
-        p.bind('<ButtonPress-1>',lambda event, p=p,nom=i:c3189.activer(nom,p))
-        p.bind('<ButtonRelease-1>',lambda event, nom=i:c3189.release(nom))
-        
-    n=n+1
-    boutonscoffrage.append(p)
-    boutons.append(p)
-
-l=0
-for i in pieds:
-    p=i
-    if n%2==1:
-        p=tk.Button(fenetre)
-        p.config(image=uparrow)
-        p.bind('<ButtonPress-1>',lambda event, p=p,nom=i:c3189.activer(nom,p))
-        p.bind('<ButtonRelease-1>',lambda event, nom=i:c3189.release(nom))
-        
-        
-    elif n%2==0:
-        p=tk.Button(fenetre)
-        p.config(image=downarrow)
-        p.bind('<ButtonPress-1>',lambda event, p=p,nom=i:c3189.activer(nom,p))
-        p.bind('<ButtonRelease-1>',lambda event, nom=i:c3189.release(nom))
-        
-    n=n+1
-    boutonspieds.append(p)
-    boutons.append(p)
 
 
 
