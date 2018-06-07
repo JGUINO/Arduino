bd0480a68b46676536e28ee9a1622ee0f4763ba1
JGUINO <jerome.guillon@cmc-coffrages.com>
2018-06-07 11:44:57 +0200

    cmd

--- chipcommande.py
+++ chipcommande.py
@@ -185,7 +185,7 @@ def affichercoffrage():
     bpie.config(bg='grey')
     tt.place_forget()
     for i in boutons:
-        i.place_forget()
+        i.button.place_forget()
     for j in plab:
         j.place_forget()
     for k in coffrage:
@@ -228,7 +228,7 @@ def afficherpieds():
     b1.place_forget()
     tt.place_forget()
     for i in boutons:
-        i.place_forget()
+        i.button.place_forget()
     for j in clab:
         j.place_forget()
     for k in boutonspieds:
