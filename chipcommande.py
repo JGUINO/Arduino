0c02544c770f378af4e45d501c83c80bfead5d5a
JGUINO <jerome.guillon@cmc-coffrages.com>
2018-06-07 16:43:37 +0200

    cmd

--- chipcommande.py
+++ chipcommande.py
@@ -90,7 +90,16 @@ class bouton():
         f1.config(bg='blue')
         b1.config(bg='blue')
         tt.config(bg='blue')
-        led.off()
+        if type(led)!=list:
+            led.off()
+        if type(led1)!=list:
+            led1.off()
+        if type(led2)!=list:
+            led2.off()
+        if type(led3)!=list:
+            led3.off()
+        if type(led4)!=list:
+            led4.off()
         led=[]
         led1=[]
         led2=[]
