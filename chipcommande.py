08fccc49ce7b2473ab1492c13542a679df97cc0d
JGUINO <jerome.guillon@cmc-coffrages.com>
2018-06-07 11:54:01 +0200

    cmd

--- chipcommande.py
+++ chipcommande.py
@@ -248,12 +248,13 @@ def afficherpieds():
 
 
 
-print (boutons)
+
 quitter=tk.Button(fenetre,text='Quitter',command=fenetre.destroy)
 quitter.place(x=725,y=440)
 
 for i in boutons:
     i.button.config(state=NORMAL)
+    print('Bouton {} setup'.format(i.nom))
 
 bcoffr=tk.Button(fenetre,text='Coffrage',font=(70),height=3,width=10,command=affichercoffrage)
 bcoffr.place(x=400,y=425)
