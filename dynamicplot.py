093dd38812ab7253cfb7cd3dcca0dae6b3624761
JGUINO <jerome.guillon@cmc-coffrages.com>
2018-06-18 10:45:21 +0200

    jauge

--- dynamicplot.py
+++ dynamicplot.py
@@ -28,8 +28,8 @@ class App_Window(tkinter.Tk):
         self.line1, = FigSubPlot.plot(x,y,'r-')
         self.canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(Fig, master=self)
         self.canvas.show()
-        self.canvas.get_tk_widget().pack(side=Tkinter.TOP, fill=tkinter.BOTH, expand=1)
-        self.canvas._tkcanvas.pack(side=Tkinter.TOP, fill=tkinter.BOTH, expand=1)
+        self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+        self.canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
         self.resizable(True,False)
         self.update()
     def refreshFigure(self,x,y):
