7921c5e938294ab5983bdd280d5e5ba28633999c
JGUINO <jerome.guillon@cmc-coffrages.com>
2018-06-20 09:52:34 +0200

    plot

--- dynamicplot.py
+++ dynamicplot.py
@@ -25,7 +25,7 @@ class App_Window(tkinter.Tk):
         FigSubPlot = Fig.add_subplot(111)
         x=[1,2,3,4]
         y=[300,870,604,330]
-        self.line1, = FigSubPlot.bar(x,y,'r-')
+        self.line1, = FigSubPlot.bar(x,y)
         self.canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(Fig, master=self)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
