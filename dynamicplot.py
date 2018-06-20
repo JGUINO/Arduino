634d3a43da3eee9aa0adfd28a86101f6f39d5c54
JGUINO <jerome.guillon@cmc-coffrages.com>
2018-06-20 10:01:58 +0200

    plot

--- dynamicplot.py
+++ dynamicplot.py
@@ -25,7 +25,7 @@ class App_Window(tkinter.Tk):
         FigSubPlot = Fig.add_subplot(111)
         x=[1,2,3,4]
         y=[300,870,604,330]
-        self.line1, = FigSubPlot.hist(x,50,density=1,facecolor='r',alpha=0.75)
+        self.line1, = FigSubPlot.hist(x)
         FigSubplot.axis([0,5,0,1000])
         self.canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(Fig, master=self)
         self.canvas.draw()
